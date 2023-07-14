09dfa4a6d7d9ee30f4f942c30d955019041b712b
AvniChauhan03 <136076288+AvniChauhan03@users.noreply.github.com>
2023-07-14 17:54:47 +0530

    Update Assessment2.sol

--- contracts/Assessment2.sol
+++ contracts/Assessment2.sol
@@ -1,1 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
+contract Assessment {
+    address payable public owner;
+    uint256 public balance;
+
+    event Deposit(address indexed sender, uint256 amount);
+    event Withdraw(address indexed sender, uint256 amount);
+
+    constructor(uint256 initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balance;
+    }
+
+    function deposit() public payable {
+        require(msg.sender == owner, "You are not the owner of this account");
+        balance += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance(balance, _withdrawAmount);
+        }
+        balance -= _withdrawAmount;
+        emit Withdraw(msg.sender, _withdrawAmount);
+        (bool success, ) = msg.sender.call{value: _withdrawAmount}("");
+        require(success, "Transfer failed");
+    }
+}
